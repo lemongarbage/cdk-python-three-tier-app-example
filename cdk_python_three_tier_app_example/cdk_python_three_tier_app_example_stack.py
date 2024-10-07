eb96493ea941d796a3fe6d07962bd225d0196799
Harrison <captainj777@gmail.com>
2024-10-06 22:10:12 -0400

    ddb table

--- cdk_python_three_tier_app_example/cdk_python_three_tier_app_example_stack.py
+++ cdk_python_three_tier_app_example/cdk_python_three_tier_app_example_stack.py
@@ -1,8 +1,11 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    StackProps,
+    RemovalPolicy,
+    aws_dynamodb as _ddb
 )
+
+
 from constructs import Construct
 
 class CdkPythonThreeTierAppExampleStack(Stack):
@@ -10,10 +13,17 @@ class CdkPythonThreeTierAppExampleStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkPythonThreeTierAppExampleQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create the DDB table
+        table = _ddb.Table(self, 'NotesTable',
+            billing_mode=_ddb.BillingMode.PAY_PER_REQUEST,
+            partition_key=_ddb.Attribute(
+                name="pk",
+                type=_ddb.AttributeType.STRING
+            ),
+            removal_policy=RemovalPolicy.DESTROY,
+            sort_key=_ddb.Attribute(
+                name="sk",
+                type=_ddb.AttributeType.STRING
+            ),
+            table_name="NotesTable"       
+            )
